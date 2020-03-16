5c5af2b54e8110871ba6e901641a0be8581eb039
Ro4052 <Ro4052@users.noreply.github.com>
2020-03-16 17:13:30 +0000

    add scroll to bottom button

--- ClientApp/src/view/MessageContainer.jsx
+++ ClientApp/src/view/MessageContainer.jsx
@@ -1,23 +1,58 @@
-import React, { useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useSelector } from "react-redux";
+import { Button } from "semantic-ui-react";
 
 import Message from "./Message";
 import styles from "./MessageContainer.module.css";
 
 export default function MessageContainer() {
+  const containerRef = useRef();
   const messageBottomRef = useRef();
   const messages = useSelector(state => state.chat.messages);
 
+  const [showToBottom, setShowToBottom] = useState(false);
+
   useEffect(() => {
-    messageBottomRef.current.scrollIntoView();
+    const element = containerRef.current;
+    const scrollListener = () => {
+      const messagesBottom = messageBottomRef.current.getBoundingClientRect()
+        .bottom;
+      const containerBottom = element.getBoundingClientRect().bottom;
+
+      if (messagesBottom > containerBottom && !showToBottom)
+        setShowToBottom(true);
+      else if (messagesBottom <= containerBottom && showToBottom)
+        setShowToBottom(false);
+    };
+    element.addEventListener("scroll", scrollListener);
+
+    return () => element.removeEventListener("scroll", scrollListener);
+  }, [showToBottom]);
+
+  useEffect(() => {
+    if (messages.slice(-1)[0]?.isMine) {
+      messageBottomRef.current.scrollIntoView();
+    }
   }, [messages]);
 
   return (
-    <div className={styles.messageContainer}>
-      {messages.map((msg, i) => (
-        <Message key={i} {...msg} />
-      ))}
-      <div ref={messageBottomRef} />
-    </div>
+    <>
+      <div ref={containerRef} className={styles.messageContainer}>
+        {messages.map((msg, i) => (
+          <Message key={i} {...msg} />
+        ))}
+        <div ref={messageBottomRef} />
+      </div>
+      {showToBottom && (
+        <div className={styles.toBottomContainer}>
+          <Button
+            circular
+            icon="angle double down"
+            size="large"
+            onClick={() => messageBottomRef.current.scrollIntoView()}
+          />
+        </div>
+      )}
+    </>
   );
 }
