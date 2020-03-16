cf36e8cdd9468907905e29524464bc10a1e08626
Ro4052 <Ro4052@users.noreply.github.com>
2020-03-16 15:08:38 +0000

    fix WebSocket setup for development

--- ClientApp/src/sockets/index.js
+++ ClientApp/src/sockets/index.js
@@ -4,11 +4,10 @@ let socket = null;
 
 export function connect(userId) {
   const wsProtocol = window.location.protocol === "http:" ? "ws" : "wss";
-  // const socketUrl =
-  //   process.env.REACT_APP_SOCKET_URL ||
-  //   `${wsProtocol}://${window.location.host}/ws`;
-  const socketUrl = `${wsProtocol}://${window.location.host}/ws`;
-  // const socketUrl = "ws://http://127.0.0.1:4392/ws";
+  const socketUrl =
+    process.env.REACT_APP_SOCKET_URL ||
+    `${wsProtocol}://${window.location.host}/ws`;
+
   socket = new WebSocket(socketUrl);
 
   socket.onopen = () => {
