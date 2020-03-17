6dc4570fff5ef8b3a9d1d4360b0aa51629690af9
Ro4052 <Ro4052@users.noreply.github.com>
2020-03-17 10:11:15 +0000

    add return to dashboard functionality

--- ClientApp/src/view/TopBar.jsx
+++ ClientApp/src/view/TopBar.jsx
@@ -1,15 +1,29 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 import { Button } from "semantic-ui-react";
 
+import { clearStore } from "../redux/user-reducer";
 import styles from "./TopBar.module.css";
 
 export default function TopBar() {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
   return (
     <div className={styles.container}>
       <div className={styles.usernameHolder}>
         <span className={styles.nameTag}>You:</span> placeholder
       </div>
-      <Button negative>Leave Chat</Button>
+      <Button
+        negative
+        onClick={() => {
+          dispatch(clearStore());
+          history.push("/dash");
+        }}
+      >
+        Leave Chat
+      </Button>
     </div>
   );
 }
