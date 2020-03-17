6c3fb06dd04c4de6e73cc36454396946de8148b3
Ro4052 <Ro4052@users.noreply.github.com>
2020-03-17 10:55:30 +0000

    add UI for changing username

---
+++ ClientApp/src/view/DynamicInput.jsx
@@ -0,0 +1,66 @@
+import React, { useState, useRef, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Input } from "semantic-ui-react";
+
+export default function DynamicInput({
+  usernameSelector,
+  labelClass,
+  formClass
+}) {
+  const username = useSelector(usernameSelector);
+  const inputRef = useRef();
+
+  const [showInput, setShowInput] = useState(false);
+  const [inputValue, setInputValue] = useState(username);
+
+  const handleClick = () => {
+    setInputValue(username);
+    setShowInput(true);
+  };
+
+  const handleChange = e => {
+    if (e.target.value.length <= 20) {
+      setInputValue(e.target.value);
+    }
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (inputValue.length > 0) {
+      console.log(inputValue);
+      setShowInput(false);
+    }
+  };
+
+  useEffect(() => {
+    const clickHandler = e => {
+      if (!e.path.includes(inputRef.current)) {
+        setShowInput(false);
+      }
+    };
+
+    if (showInput) {
+      document.addEventListener("click", clickHandler);
+    }
+
+    return () => document.removeEventListener("click", clickHandler);
+  }, [inputRef, showInput]);
+
+  return (
+    <>
+      {showInput ? (
+        <form ref={inputRef} className={formClass} onSubmit={handleSubmit}>
+          <Input
+            placeholder="Username..."
+            value={inputValue}
+            onChange={handleChange}
+          />
+        </form>
+      ) : (
+        <span className={labelClass} onClick={handleClick}>
+          {username}
+        </span>
+      )}
+    </>
+  );
+}
--- ClientApp/src/view/TopBar.jsx
+++ ClientApp/src/view/TopBar.jsx
@@ -1,20 +1,25 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { Button } from "semantic-ui-react";
 
 import { clearStore } from "../redux/user-reducer";
+import DynamicInput from "./DynamicInput";
 import styles from "./TopBar.module.css";
 
 export default function TopBar() {
-  const username = useSelector(state => state.user.username);
   const dispatch = useDispatch();
   const history = useHistory();
 
   return (
     <div className={styles.container}>
       <div className={styles.usernameHolder}>
-        <span className={styles.nameTag}>You:</span> {username}
+        <span className={styles.nameTag}>You:</span>{" "}
+        <DynamicInput
+          usernameSelector={state => state.user.username}
+          labelClass={styles.username}
+          formClass={styles.usernameForm}
+        />
       </div>
       <Button
         negative
